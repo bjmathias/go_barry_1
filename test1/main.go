5344de5fc40122b80bd96bac34b9af61e0abe111
bjmathias <bjm@technologist.com>
2018-10-26 14:40:54 +0100

    Update test1/main.go

--- test1/main.go
+++ test1/main.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+// Global defiitions/declarations needs to be long form
+var intglobal int32 = 8899
+
+var intglobal2 int32
+
 func main() {
 
+	intglobal2 = 7766
+
+	// Shorthand notation only valid within function.
 	charvar := "Barry was here"
 
 	intvar := 11223344
 	intvar2 := 11223344
+	var i32var int32 = 55667788
 
-	fmt.Printf("%s %s %d", charvar, " ", intvar)
+	fmt.Printf("%s %s %d %d", charvar, " ", intvar, i32var)
 
 	if intvar == intvar2 {
 		fmt.Println("Ints are identical")
 	}
+	sliceTest := []string{"one", "two", "three"}
+	fmt.Println(sliceTest)
+
+	// Short form map
+	testMap := map[string]string{"one": "1", "two": "2", "three": "3"}
+
+	fmt.Println(testMap)
+
+	// remove map entry
+	delete(testMap, "two")
+
+	fmt.Println(testMap)
 }
