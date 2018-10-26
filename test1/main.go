9f2e7e806146af82ed638ac32c99c7b9adee6c9b
bjmathias <bjm@technologist.com>
2018-10-26 12:12:53 +0100

    Update test1/main.go

---
+++ test1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	charvar := "Barry was here"
+
+	intvar := 11223344
+	intvar2 := 11223344
+
+	fmt.Printf("%s %s %d", charvar, " ", intvar)
+
+	if intvar == intvar2 {
+
+	}
+}
